9d330213af39aa9f9a22d24e3797a59e3f9b6377
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2019-07-07 00:58:01 +0300

    added sort

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,47 @@
+package sort;
+
+import java.util.Arrays;
+
+
+public class QuickSort {
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        int prop= array[middle];
+
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (array[i] < prop)
+                i++;
+            while (array[j] > prop)
+                j--;
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+            quickSort(array,low, j);
+        if (high > i)
+            quickSort(array, i, high);
+    }
+
+    /*test*/
+    public static void main(String[] args) {
+        int x [] ={12,5,3,53,5,1};
+        System.out.println(Arrays.toString(x));
+        int low =0;
+        int high = x.length-1;
+        quickSort(x,low,high);
+        System.out.println(Arrays.toString(x));
+    }
+}
