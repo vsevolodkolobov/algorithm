3ad2356022677e1eebad6ad1eb9da7c4eb09bcef
Vsevolod Kolobov <42353318+vsevolodkolobov@users.noreply.github.com>
2020-02-20 23:25:39 +0300

    update selection sort

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -5,21 +5,16 @@ import java.util.Arrays;
 public class SelectionSort {
 
     public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int min = arr[i];
-            int min_i = i;
-
+             int tmp, min;
+        for (int i = 0; i < arr.length - 1; i++) {
+            min = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    min_i = j;
-                }
+                if (arr[j] < arr[min])
+                    min = j;
             }
-            if (i != min_i) {
-                int tmp = arr[i];
-                arr[i] = arr[min_i];
-                arr[min_i] = tmp;
-            }
+            tmp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = tmp;
         }
     }
 
