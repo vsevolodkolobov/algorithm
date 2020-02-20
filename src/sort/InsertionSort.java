f22d33f9d29eabf27f0ef1531105dd310136edf2
Vsevolod Kolobov <42353318+vsevolodkolobov@users.noreply.github.com>
2020-02-20 23:31:26 +0300

    added 2 way for insert sort

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-    public static void insertionSort(int[] arr) {
+    public static void insertionSort1(int[] arr) {
 
         for (int i=1; i<arr.length;i++) {
             int curElem = arr[i];
@@ -18,13 +18,27 @@ public class InsertionSort {
         }
     }
 
+    // 2 way 
+    static void insSort2(int arr[]) {
+
+        int current;
+        for (int i = 1; i < arr.length; i++) {
+            current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                    arr[j + 1] = arr[j];
+                    j--;
+            }
+            arr[j + 1] = current;
+        }
+    }
 
 
     /*test*/
     public static void main(String[] args) {
             int arr1[] = {1,7,3,2,6,3};
             System.out.print(Arrays.toString(arr1));
-            insertionSort(arr1);
+            insertionSort1(arr1);
             System.out.println(Arrays.toString(arr1));
 
 
